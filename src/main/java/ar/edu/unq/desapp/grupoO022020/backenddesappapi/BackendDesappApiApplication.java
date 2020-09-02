49a9c12161cb6ad91aaffc1e8c1d9176df205c18
Martin <castello.martin@yahoo.com.ar>
2020-09-01 21:23:38 -0300

    Commit inicial

---
+++ src/main/java/ar/edu/unq/desapp/grupoO022020/backenddesappapi/BackendDesappApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoO022020.backenddesappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesappApiApplication.class, args);
+	}
+
+}
