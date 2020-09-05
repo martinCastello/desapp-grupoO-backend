6dee768e75bdd223b1bf5a01a9edbfe3c7494874
Martin <castello.martin@yahoo.com.ar>
2020-09-05 20:46:28 -0300

    Add executable for run docker

---
+++ dockerExecute.sh
@@ -0,0 +1,20 @@
+#Check if docker is installed
+if [ $(which docker) ]
+then
+    echo "Initial docker steps"
+else
+    echo "You most need Docker"
+    exit 0
+fi
+
+# Check if image is created
+if [ -n $(docker images -q desapp-grupo-o-backend) ]
+then
+    echo "Using existing image"
+else
+    echo "Create image"
+    docker build build --build-arg JAR_FILE=build/libs/*.war -t springio/desapp-grupo-o-backend .
+    echo "Success create image"
+fi
+
+docker run -p 8080:8080 springio/desapp-grupo-o-backend
