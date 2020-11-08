c1ef0f1d81755b5c764046de6b99a0be8c39e4af
Martin <castello.martin@yahoo.com.ar>
2020-11-08 15:53:52 -0300

    Delete info mail

--- src/main/java/ar/edu/unq/desapp/grupoo022020/backenddesappapi/service/impl/EmailServiceImpl.java
+++ src/main/java/ar/edu/unq/desapp/grupoo022020/backenddesappapi/service/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package ar.edu.unq.desapp.grupoo022020.backenddesappapi.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -15,7 +16,8 @@ public class EmailServiceImpl implements EmailService {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmailServiceImpl.class);
 
-	private static final String NOREPLY_ADDRESS = "castellomn94@gmail.com";
+	@Value("${spring.mail.username}")
+	private static String NOREPLY_ADDRESS;
 	
 	@Autowired
     private JavaMailSender emailSender;
